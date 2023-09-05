3302affed48df5471c9048e2db902fda30a73278
Nataniel Carrasquillo <nataniel-carras@hotmail.com>
2023-09-05 12:23:17 -0500

    completed js and html. added to readme. fixed file organization

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,43 +4,37 @@
 
 $(function() {
 
-  var saveButton = $('saveBtn');
+  var saveButton = $('.saveBtn');
   var currentDay = $('#currentDay');
   var currentTime = dayjs().hour();
   var timeBlocks = $('#schedule').children('.time-block');
   currentDay.text(dayjs().format('dddd, MMMM DD'));
-  console.log(currentTime);
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  console.log(saveButton);
+  console.log(localStorage);
+  
   // When clicking on save button
   saveButton.on('click', function() {
     // Assign selectedTime to be the id for the div in which the save button is in
     selectedTime = $(this).parents('.time-block').attr('id');
-    writtenTask = $(this).siblings('.description').text();
+    writtenTask = $(this).siblings('.description').val();
+    console.log('clicked save');
     console.log(selectedTime);
     console.log(writtenTask);
     localStorage.setItem(JSON.stringify(selectedTime), JSON.stringify(writtenTask));
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // For all time blocks do:
   timeBlocks.each(function() {
-    timeSlot = $(this).attr('id');
-    slotHour = timeSlot.substr(5,2);
-    console.log(slotHour);
+    var timeSlot = $(this).attr('id');
+    var slotHour = timeSlot.substr(5,2);
+    
+    // Load in the previously saved tasks that are saved in local storage
+    var storedTask = JSON.parse(localStorage.getItem(JSON.stringify(timeSlot)));
+    console.log(storedTask);
+    if (storedTask !== null) {
+      $(this).children('.description').val(storedTask);
+    }
+
     // if time block is in the past based on the current time
     if (dayjs().isBefore(dayjs().hour(slotHour), 'hour')) {
     // Make the class of the current time block be "past"
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Work Day Scheduler
 
 ## Description
+This code is used to create a work day scheduling application. The application allows the user to enter tasks into specific timeslots for a day and save these tasks to the local storage, allowing the application to remember the user's saved tasks
 
 ## Website Link
+https://nataniel-c.github.io/work-day-scheduler
 
-## GitHub Repository
+## Website Screenshots
+
